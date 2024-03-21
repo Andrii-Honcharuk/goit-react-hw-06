292b3f0b3b7eb9aacc63e05423f64a29e8331b1b
Andrii Honcharuk <goncharukam@gmail.com>
2024-03-21 18:39:15 +0200

    add_localStorage

--- src/redux/filtersSlice.js
+++ src/redux/filtersSlice.js
@@ -1,25 +1,26 @@
-const filters = {
-  name: "",
-}
+// filtersSlice.js
 
-export const filtersReducer = (state = filters, action) => {
-  console.log("Action", action);
-  console.log("State", state);
-  switch (action.type) {
-    case "filters/filterContacts":
-      return {
-        ...state,
-        name: action.payload,
-      };
-      
-    default:
-      return state;
+import { createSlice } from "@reduxjs/toolkit";
+
+// const filters = {
+//   name: "",
+// }
+
+const slice = createSlice({
+  name: "filters",
+  initialState: {
+    name: ""
+  },
+  reducers: {
+    filterContactsByName(state, action) { 
+      state.name = action.payload;
+    }
   }
-};
+})
 
-export const filterContactsByName = (name) => {
-  return {
-    type: "filters/filterContacts",
-    payload: name,
-  };
-};
+export default slice.reducer;
+
+export const { filterContactsByName } = slice.actions;
+
+
+
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,37 +1,24 @@
 //store.js
-
-// import { configureStore } from "@reduxjs/toolkit";
-import { createStore } from "redux";
-// import { createStore } from "@reduxjs/toolkit";
-import { devToolsEnhancer } from "@redux-devtools/extension";
-import { combineReducers } from "redux";
-import { contactsReducer } from "./contactsSlice";
-import { filtersReducer } from "./filtersSlice";
+import { configureStore } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
+import  contactsReducer  from "./contactsSlice";
+import filtersReducer from "./filtersSlice";
 
 
-export const rootReducer = combineReducers({
-  contacts: contactsReducer,
-  filters: filtersReducer,
-})
+const contactsPersistConfig = {
+  key: 'contacts',
+  storage,
+  whitelist: ["contacts"]
+};
 
+const persistedContactsReducer = persistReducer(contactsPersistConfig, contactsReducer)
 
+export const store = configureStore({
+  reducer: {
+    contacts: persistedContactsReducer,
+    filters: filtersReducer,
+  }
+});
 
-
-export const store = createStore(rootReducer, devToolsEnhancer())
-
-
-
-// export const store = configureStore({
-//   reducer: {
-          
-//   contacts:     [
-//     { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-//     { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-//     { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-//     { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
-// ],
-//   locale: {
-//     lang: 'ua',
-//      },
-// }
-// });
+export const persistor = persistStore(store);
